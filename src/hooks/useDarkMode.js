5cab4b31b7d2e1b7c91a62a229fc6c214728b6dc
CarlosSantiago Nevett <csnevett@gmail.com>
2021-03-10 18:08:23 -0500

    added system preferences

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,1 +1,20 @@
 import useLocalStorage from './useLocalStorage';
+import { useEffect } from 'react';
+import useMedia from './useMedia';
+
+const useDarkMode = () => {
+  const [isDark, setIsDark] = useLocalStorage('darkMode');
+  const prefersDarkMode = usePrefersDarkMode();
+
+  useEffect(() => {
+    setIsDark(typeof isDark !== 'undefined' ? isDark : prefersDarkMode);
+  }, []);
+
+  return [isDark, setIsDark];
+};
+
+function usePrefersDarkMode() {
+  return useMedia(['(prefers-color-scheme: dark)'], [true], false);
+}
+
+export default useDarkMode;
